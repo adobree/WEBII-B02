e635cae57191d9c4147d849f608260d423473c2d
Adam Kovacs <ado.kovacs@gmail.comm>
2022-10-29 09:01:57 +0200

    V1

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+//alkalmaz�s gy�k�r k�nyvt�ra a szerveren
+define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'/web2kovacsadam/');
+//URL c�m az alkalmaz�s gy�ker�hez
+define('SITE_ROOT', 'http://pfw.ddns.net/web2kovacsadam/');
+
+// a router.php vez�rl� bet�lt�se
+require_once(SERVER_ROOT . 'controllers/' . 'router.php');
+
+?>
