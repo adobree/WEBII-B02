85accc9cb7e683a3214154df6900651477f9f1b7
Gábor Nagy <gabor.nagy@ewmgroup.com>
2022-11-21 19:48:23 +0100

    new branch

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 //alkalmaz�s gy�k�r k�nyvt�ra a szerveren
-define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'/web2kovacsadam/');
+define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/WEBII-B02/');
+define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/WEBII-B02/');
 //URL c�m az alkalmaz�s gy�ker�hez
-define('SITE_ROOT', 'http://pfw.ddns.net/web2kovacsadam/');
+// define('SITE_ROOT', 'http://pfw.ddns.net/web2kovacsadam/');
 
 // a router.php vez�rl� bet�lt�se
 require_once(SERVER_ROOT . 'controllers/' . 'router.php');
-
-?>
--- views/page_main.php
+++ views/page_main.php
@@ -1,25 +1,70 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>MVC - PHP</title>
-        <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT?>css/main_style.css">
-        <?php if($viewData['style']) echo '<link rel="stylesheet" type="text/css" href="'.$viewData['style'].'">'; ?>
-    </head>
-    <body>
-        <header>
-            <div id="user"><em><?= $_SESSION['userlastname']." ".$_SESSION['userfirstname'] ?></em></div>
-            <h1 class="header">Web-programozás II - MVC alkalmazás</h1>
-        </header>
-        <nav>
-            <?php echo Menu::getMenu($viewData['selectedItems']); ?>
+
+<head>
+    <meta charset="utf-8">
+    <title>MVC - PHP</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT ?>css/main_style.css">
+    <?php if ($viewData['style']) echo '<link rel="stylesheet" type="text/css" href="' . $viewData['style'] . '">'; ?>
+</head>
+
+<body>
+    <header>
+        <div id="user"><em><?= $_SESSION['userlastname'] . " " . $_SESSION['userfirstname'] ?></em></div>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                Web-programozás II - MVC alkalmazás
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- <ul class="navbar-nav mr-auto"> -->
+                <?php echo Menu::getMenu($viewData['selectedItems']); ?>
+                <!-- <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li> -->
+                <!-- </ul> -->
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
         </nav>
-        <aside>
-                <p>Phasellus wisi nulla...</p>
-        </aside>
-        <section>
-            <?php if($viewData['render']) include($viewData['render']); ?>
-        </section>
-        <footer>&copy; NJE - GAMF - Informatika Tanszék <?= date("Y") ?></footer>
-    </body>
+    </header>
+    <!-- <nav>
+        <?php echo Menu::getMenu($viewData['selectedItems']); ?>
+    </nav> -->
+    <aside>
+        <p>Phasellus wisi nulla...</p>
+    </aside>
+    <section>
+        <?php if ($viewData['render']) include($viewData['render']); ?>
+    </section>
+    <footer>&copy; NJE - GAMF - Informatika Tanszék <?= date("Y") ?></footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+</body>
+
 </html>
