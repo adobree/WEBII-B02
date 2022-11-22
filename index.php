9fb76da5bb3af850cf8d0183ffe70547ad7ebb8a
Gábor Nagy <gabor.nagy@ewmgroup.com>
2022-11-22 20:45:07 +0100

    fix route issue

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 //alkalmaz�s gy�k�r k�nyvt�ra a szerveren
 define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/WEBII-B02/');
-define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/WEBII-B02/');
-//URL c�m az alkalmaz�s gy�ker�hez
+define('URLI', '');
 // define('SITE_ROOT', 'http://pfw.ddns.net/web2kovacsadam/');
+define('SITE_ROOT', '');
+//URL c�m az alkalmaz�s gy�ker�hez
+
 
 // a router.php vez�rl� bet�lt�se
 require_once(SERVER_ROOT . 'controllers/' . 'router.php');
