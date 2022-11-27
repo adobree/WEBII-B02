af63907fad9e232bd46dfa6cae2f3b6a98a37f84
Gábor Nagy <gabor.nagy@ewmgroup.com>
2022-11-27 21:54:43 +0100

    request for süti

---
+++ models/sutemeny_model.php
@@ -0,0 +1,79 @@
+<?php
+require_once '../includes/database.inc.php';
+$suti = $_REQUEST["q"];
+$response = "";
+try {
+    // echo $suti;
+    $connection = Database::getConnection();
+    $sql = "select suti.nev from cukraszda.suti where suti.nev like '%" . $suti . "%'";
+    // $sql = "select nev from suti where nev like '" . $suti . "'";
+    $stmt = $connection->query($sql);
+    $suti_res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // echo $suti_res[0]["nev"];
+    $talalat = [];
+    foreach ($suti_res as $suti) {
+        echo $suti["nev"];
+        // echo var_dump($suti);
+        array_push($talalat, $suti);
+    }
+    echo "találat: " . var_dump($talalat);
+    return json_encode($talalat);
+    // echo var_dump($talalat);
+    // echo var_dump($suti);
+} catch (PDOException $error) {
+    echo "kutyageci";
+    $response = "Hiba";
+}
+// Array with names
+// $a[] = "Anna";
+// $a[] = "Brittany";
+// $a[] = "Cinderella";
+// $a[] = "Diana";
+// $a[] = "Eva";
+// $a[] = "Fiona";
+// $a[] = "Gunda";
+// $a[] = "Hege";
+// $a[] = "Inga";
+// $a[] = "Johanna";
+// $a[] = "Kitty";
+// $a[] = "Linda";
+// $a[] = "Nina";
+// $a[] = "Ophelia";
+// $a[] = "Petunia";
+// $a[] = "Amanda";
+// $a[] = "Raquel";
+// $a[] = "Cindy";
+// $a[] = "Doris";
+// $a[] = "Eve";
+// $a[] = "Evita";
+// $a[] = "Sunniva";
+// $a[] = "Tove";
+// $a[] = "Unni";
+// $a[] = "Violet";
+// $a[] = "Liza";
+// $a[] = "Elizabeth";
+// $a[] = "Ellen";
+// $a[] = "Wenche";
+// $a[] = "Vicky";
+
+// // get the q parameter from URL
+// // $suti = $_REQUEST["q"];
+
+// // $hint = "";
+
+// // if ($suti !== "") {
+// //     $suti = strtolower($suti);
+// //     $len = strlen($suti);
+// //     foreach ($a as $name) {
+// //         if (stristr($suti, substr($name, 0, $len))) {
+// //             if ($hint === "") {
+// //                 $hint = $name;
+// //             } else {
+// //                 $hint .= ", $name";
+// //             }
+// //         }
+// //     }
+// // }
+
+// // // Output "no suggestion" if no hint was found or output correct values
+// // echo $hint === "" ? "no suggestion" : $hint;
--- views/muveletek_main.php
+++ views/muveletek_main.php
@@ -1,6 +1,42 @@
+<script>
+    class Sutemeny {
+        constructor(suti) {
+            this.suti = suti
+        }
+
+    }
+
+    function showHint(str) {
+        console.log(str)
+        if (str.length == 0) {
+            console.log("if ág")
+            document.getElementById("eredmeny").innerHTML = "";
+            return;
+        } else {
+            console.log("else ág")
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log("RES: ", this)
+                    document.getElementById("eredmeny").innerHTML = this.responseText;
+                }
+            }
+            xmlhttp.open("GET", "models/sutemeny_model.php?q=" + str, true);
+            xmlhttp.send();
+        }
+    }
+</script>
+
 <div class="row w-100 m-0 mt-1">
     <div class="col col-12 muveletek-bg p-5">
         <h1 class="text-center mt-3 primary-text">Műveletek oldal</h1>
         <img src="images/home_bg.png" alt="">
+        <form action="">
+            <div class="d-flex flex-column w-50">
+                <label for=" fname">Keressen süteményt:</label>
+                <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
+                <div id="eredmeny"></div>
+            </div>
+        </form>
     </div>
 </div>
